228ad00ce56899b22e82b07fd68f7b930b5df8d8
anilnadipannagari <111619322+anilnadipannagari@users.noreply.github.com>
2022-12-26 10:50:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,87 @@
 const userData = [
-    {id:1,name:"john",age:"18",profession:"developer"},
-    {id:2, name:"jack",age:"20", profession:"developer"},
-    {id:3, name:"karen", age:"19",profession:"admin"}
-];
-
-userData.map( function iterator(item){
-    item.age = Number(item.age);
-});
-console.log(userData);
-let result = document.getElementById("result");
-let dropdown = document.getElementById("dropdown");
-let button = document.getElementById("btn");
-function filterByProfession(){
-    result.innerHTML="";
-    let dropdownValue = dropdown.value;
-    console.log(dropdownValue);
-    userData.forEach((item) =>{
-        if(dropdownValue === "profession"){
-            alert("Please select a profession");
-            return;
-        }
-        if(item.profession === dropdownValue){
-        let div = document.createElement("div");
-        div.innerText = item.id + "  Name:" + item.name + "  Profession:" + item.profession + "  Age:" + item.age;
-        result.append(div);
-        }
-    })
-
-}
-
-button.addEventListener('click', filterByProfession)
-const tname = document.querySelector("#tname");
-const tpname = document.querySelector("#tpname");
-const taname = document.querySelector("#taname");
-const abutton = document.querySelector("#ad");
-const cartBody = document.querySelector(".cart-body");
-let cartItem = {};
-const updateCart = () =>{
-    var tr1= document.createElement("tr");
-    var tr2= document.createElement("tr");
-    var tr3= document.createElement("tr");
-    tr1.innerHTML = `<td>${cartItem.tname}</td><td>${cartItem.tpname}</td><td>${cartItem.taname}`;
-    cartBody.appendChild(tr1);
-};
+    {id:1, Name:"John", age: "18", profession:"developer"},
+    {id:2, Name:"Jack", age: "20", profession:"developer"},
+    {id:3, Name:"Karen", age: "19", profession:"admin"}
+ ]
  
-const addItemToCart = () =>{
-    cartItem = {
-        tname: tname.value,
-        tpname: tpname.value,
-        taname: taname.value,
+ // create a card to add to the filter compinents
+ function createCard(item){
+ 
+     let span1 = document.createElement("span");
+     span1.innerText = item.id+". ";
+ 
+     let span2 = document.createElement("span");
+     span2.innerText = "Name: "+item.Name[0].toUpperCase()+item.Name.slice(1);
+ 
+     let span3 = document.createElement("span");
+     span3.innerText = "Profession: "+ item.profession[0].toUpperCase()+item.profession.slice(1);
+ 
+     let span4 = document.createElement("span");
+     span4.innerText = "Age: "+ item.age;
+ 
+     let newCard = document.createElement("div");
+     newCard.append(span1,span2,span3,span4);
+     newCard.setAttribute("class","card");
+ 
+     return newCard;
+ 
+ }
+ 
+ let filterBtn =document.getElementById("filterBtn");
+ let cardsection = document.getElementById("cards");
+ 
+ // Filter button method to filter the components
+ filterBtn.onclick = ()=>{
+    let dropdown = document.getElementById("selectProfession");
+    if(dropdown.value=="developer" || dropdown.value =="admin"){
+     cardsection.innerHTML="";
+ 
+     userData.forEach((item)=>{
+         if(item.profession==dropdown.value){
+             let newCard = createCard(item);
+             cardsection.append(newCard);
+         }
+     })
+ 
     }
-    updateCart();
-};
-abutton.addEventListener("click",addItemToCart);
+    else{
+     alert("Please select a profession before clicking the button")
+    }
+ }
+ 
+ // To add user on clicking on add user button
+ 
+ let inputName = document.getElementById("nameInput");
+ let inputProfession = document.getElementById("professionInput");
+ let inputAge = document.getElementById("ageInput");
+ 
+ let addUserBtn = document.getElementById("addUserBtn");
+ 
+ addUserBtn.onclick = addUser;
+ 
+ function addUser(){
+     let cName = inputName.value;
+     let cProfession = inputProfession.value;
+     let cAge = inputAge.value;
+ 
+     if(cName=="" || cProfession=="" || cAge==""){
+         alert("Enter all Details")
+ 
+     }
+     else{
+         let user = {
+             id:userData.length+1,
+             Name:cName,
+             age:cAge,
+             profession:cProfession,
+         }
+         userData.push(user);
+ 
+         let newCard = createCard(user);
+         cardsection.append(newCard);
+         console.log(userData);
+ 
+     }
+   
+ 
+ }
